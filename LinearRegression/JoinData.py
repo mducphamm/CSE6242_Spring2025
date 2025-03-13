51d763480430c695406b1ea286f70582eca6faa6
Kshitij Gurung <kshitijgurung@MacBookPro.lan>
2025-03-13 13:45:00 -0500

    included holiday data into the joindata

--- LinearRegression/JoinData.py
+++ LinearRegression/JoinData.py
@@ -1,7 +1,18 @@
 import pandas as pd
+import datetime
+from azureml.opendatasets import PublicHolidays # You need to pip install azureml-opendatasets 
+from datetime import datetime
+from dateutil import parser
+from dateutil.relativedelta import relativedelta
+import pandas as pd
+
+end_date = datetime.today()
+start_date = datetime.today() - relativedelta(months=51)
+hol = PublicHolidays(start_date=start_date, end_date=end_date)
+hol_df = hol.to_pandas_dataframe()
 
 # Replace this with the path to your CSV file
-file_path = './Weather/weather_data.csv'
+file_path = '../Weather/weather_data.csv'
 
 # Read the CSV into a DataFrame, parse the 'date' column
 df = pd.read_csv(file_path, parse_dates=['date'])
@@ -36,13 +47,29 @@ df = df.astype({
     'weekend': 'int64'
 })
 
-rider_df = pd.read_csv("./ridership.csv", parse_dates=['date'])
+rider_df = pd.read_csv("../ridership.csv", parse_dates=['date'])
 
 # Aggregate the 'ridership' by 'date'
 aggregated_df = rider_df.groupby('date')['ridership'].sum().reset_index()
 
 df['date'] = pd.to_datetime(df['date'])
 aggregated_df['date'] = pd.to_datetime(aggregated_df['date'])
-merged_df = pd.merge(df, aggregated_df, on='date', how='left')
-output_file = "./joined_data.csv"
-merged_df.to_csv(output_file, index=False)
+daily_weather_data = pd.merge(df, aggregated_df, on='date', how='left')
+
+# pulling holiday data from Azure opendataset
+us_hol_df = hol_df[hol_df['countryOrRegion'] =='United States']
+us_hol_clean_df = us_hol_df.sort_values(by = 'date', ascending=True)
+us_hol_clean_df = us_hol_clean_df[(us_hol_clean_df['date'] >= '2021-06-01') & (us_hol_clean_df['date'] < '2025-01-01')]
+us_hol_clean_df = us_hol_clean_df[['holidayName', 'date']]
+
+# merging into daily weather data
+daily_weather_data['date'] = pd.to_datetime(daily_weather_data['date'])
+us_hol_clean_df['date'] = pd.to_datetime(us_hol_clean_df['date'])
+daily_RWH_df = daily_weather_data.merge(us_hol_clean_df, how='left', on = 'date') #RWH: Ridership Weather Holiday
+daily_RWH_df['holidayName'] = daily_RWH_df['holidayName'].fillna('No holiday')
+daily_RWH_df['is_holiday'] = daily_RWH_df['holidayName'].apply(lambda x: 1 if x != 'No holiday' else 0)
+
+# exporting daily ridership weather holiday data
+output_file = "./daily_ridership_weather_holi_data.csv"
+daily_RWH_df.to_csv(output_file, index=False)
+
