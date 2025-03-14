76a85efcdb5f48545765aa9237ffb0a42252addb
Minh Duc Pham <phamminhduckl@gmail.com>
2025-03-14 00:52:18 -0500

    Basic struture of backend and front-end

---
+++ weather-prediction/src/App.js
@@ -0,0 +1,12 @@
+import Tab from "./Tab";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Tab/>
+    </div>
+  );
+}
+
+export default App;
---
+++ weather-prediction/src/Tableau.js
@@ -0,0 +1,5 @@
+const Tableau = () =>{
+    return <div>This is the content for Tab 2</div>;
+}
+
+export default Tableau;
